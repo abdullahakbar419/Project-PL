1416187f69747ddc98ff21da8fce8968a3a726d8
Abdullah Akbar <abdullahakbar419@gmail.com>
2022-09-07 17:14:31 +0700

    correction

--- resources/views/partials/header_before.blade.php
+++ resources/views/partials/header_before.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Berita Acara PPIG</title>
     <meta content="" name="description">
